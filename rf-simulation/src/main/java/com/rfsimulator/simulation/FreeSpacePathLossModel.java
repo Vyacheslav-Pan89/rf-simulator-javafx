90d5f583e012d79c2e3ecd5fa7ed56336ccd3e28
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2026-06-24 17:49:33 +0300

    milestone-6 implemented

--- rf-simulation/src/main/java/com/rfsimulator/simulation/FreeSpacePathLossModel.java
+++ rf-simulation/src/main/java/com/rfsimulator/simulation/FreeSpacePathLossModel.java
@@ -1,5 +1,6 @@
 package com.rfsimulator.simulation;
 
+import com.rfsimulator.domain.Distance;
 import com.rfsimulator.domain.Position2D;
 import com.rfsimulator.domain.PowerDbm;
 import com.rfsimulator.scene.RadioSource;
@@ -7,15 +8,26 @@ import com.rfsimulator.scene.RadioSource;
 import java.util.Objects;
 
 public class FreeSpacePathLossModel implements PropagationModel {
+
+    private static final double METERS_PER_KILOMETER = 1_000.0;
+    private static final double HERTZ_PER_MEGAHERTZ = 1_000_000.0;
+    private static final double FSPL_CONSTANT_FOR_KM_AND_MHZ = 32.44;
+
     @Override
     public PowerDbm calculatePower(RadioSource radioSource, Position2D position) {
         Objects.requireNonNull(radioSource, "radioSource");
         Objects.requireNonNull(position, "position");
-
         if (radioSource.position().equals(position)) {
             throw new IllegalArgumentException("Radio source cannot be at the same position as the receiver");
         }
 
-        return null;
+        Distance distance = radioSource.position().distanceTo(position);
+        double freeSpaceLoss = 20 * Math.log10(distance.meters() / METERS_PER_KILOMETER)
+                + 20 * Math.log10(radioSource.frequency().hertz() / HERTZ_PER_MEGAHERTZ)
+                + FSPL_CONSTANT_FOR_KM_AND_MHZ;
+
+        double powerAtReceiver = radioSource.transmitPower().toDbm().dbm() - freeSpaceLoss;
+
+        return new PowerDbm(powerAtReceiver);
     }
 }
--- rf-simulation/src/test/java/com/rfsimulator/simulation/FreeSpacePathLossModelTest.java
+++ rf-simulation/src/test/java/com/rfsimulator/simulation/FreeSpacePathLossModelTest.java
@@ -2,42 +2,82 @@ package com.rfsimulator.simulation;
 
 import com.rfsimulator.domain.Frequency;
 import com.rfsimulator.domain.Position2D;
+import com.rfsimulator.domain.PowerDbm;
 import com.rfsimulator.domain.PowerWatts;
 import com.rfsimulator.scene.RadioSource;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FreeSpacePathLossModelTest {
 
+    private final RadioSource testRadioSource =
+            new RadioSource(
+                    new Position2D(0.0, 0.0),
+                    new PowerWatts(1.0),
+                    new Frequency(100000000.0));
+
+    private final Position2D testTargetPosition = new Position2D(1000.0, 0.0);
+
     @Test
     public void testEqualPositionsThrowsException() {
-        RadioSource radioSource =
-                new RadioSource(
-                        new Position2D(5, 5),
-                        new PowerWatts(100),
-                        new Frequency(1000));
-
         assertThrows(IllegalArgumentException.class, () ->
                 new FreeSpacePathLossModel()
-                        .calculatePower(radioSource, new Position2D(5, 5)));
+                        .calculatePower(testRadioSource, testRadioSource.position()));
     }
 
     @Test
     public void testThrowsNullPointerExceptionWhenNullRadioSource() {
         assertThrows(NullPointerException.class, () ->
-                new FreeSpacePathLossModel().calculatePower(null, new Position2D(5, 5)));
+                new FreeSpacePathLossModel().calculatePower(null, testTargetPosition));
     }
 
     @Test
     public void testThrowsNullPointerExceptionWhenNullTargetPosition() {
-        RadioSource radioSource =
-                new RadioSource(
-                        new Position2D(5, 5),
-                        new PowerWatts(100),
-                        new Frequency(1000));
-
         assertThrows(NullPointerException.class, () ->
-                new FreeSpacePathLossModel().calculatePower(radioSource, null));
+                new FreeSpacePathLossModel().calculatePower(testRadioSource, null));
+    }
+
+    @Test
+    public void calculatesReceivedPowerForReferenceCase1() {
+        FreeSpacePathLossModel freeSpacePathLossModel = new FreeSpacePathLossModel();
+        PowerDbm powerDbm = freeSpacePathLossModel.calculatePower(testRadioSource, testTargetPosition);
+        assertEquals(-42.44, powerDbm.dbm(), 1e-12);
+    }
+
+    @Test
+    public void calculatesReceivedPowerForReferenceCase2() {
+        FreeSpacePathLossModel freeSpacePathLossModel = new FreeSpacePathLossModel();
+        PowerDbm powerDbm = freeSpacePathLossModel
+                .calculatePower(new RadioSource(
+                                new Position2D(0.0, 0.0),
+                                new PowerWatts(10.0),
+                                new Frequency(100_000_000.0)),
+                        new Position2D(0.0, 1000.0));
+
+        assertEquals(-32.44, powerDbm.dbm(), 1e-12);
+    }
+
+    @Test
+    public void calculatesReceivedPowerForReferenceCase3() {
+        FreeSpacePathLossModel freeSpacePathLossModel = new FreeSpacePathLossModel();
+        PowerDbm powerDbm = freeSpacePathLossModel
+                .calculatePower(new RadioSource(
+                                new Position2D(0.0, 0.0),
+                                new PowerWatts(1.0),
+                                new Frequency(100_000_000.0)),
+                        new Position2D(2000.0, 0.0));
+        assertEquals(-48.46059991327962, powerDbm.dbm(), 1e-12);
+    }
+
+    @Test
+    public void zeroSourcePowerThrowsException() {
+        assertThrows(IllegalStateException.class, () ->
+                new FreeSpacePathLossModel().calculatePower(new RadioSource(
+                                new Position2D(0.0, 0.0),
+                                new PowerWatts(0.0),
+                                new Frequency(100_000_000.0)),
+                        testTargetPosition));
     }
 }
